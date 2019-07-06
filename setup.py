e1a40bc2d517a40cb7b0b870094c0ad672c95a2a
Josh Wilson <person142@users.noreply.github.com>
2019-07-05 19:25:06 -0700

    Set up CircleCI

--- setup.py
+++ setup.py
@@ -10,5 +10,13 @@ EXT_MODULES = [
 ]
 
 setup(
-    ext_modules = cythonize(EXT_MODULES)
+    name='numba_special',
+    description="Numba overloads for SciPy's special functions",
+    version='0.1',
+    author='Josh Wilson',
+    url='https://github.com/person142/numba_special',
+    packages=['numba_special'],
+    package_data={'numba_special': ['signatures.json']},
+    ext_modules = cythonize(EXT_MODULES),
+    install_requires=['numba', 'scipy']
 )
